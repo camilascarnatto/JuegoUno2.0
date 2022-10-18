75704c6ef17fb09d10940ac6a11e989ee94c663d
camilascarnatto <scarnattocamila@gmail.com>
2022-10-18 15:56:33 -0300

    Cada efecto tiene su puntaje

--- src/modelo/EfectosCartaUno.java
+++ src/modelo/EfectosCartaUno.java
@@ -1,9 +1,19 @@
 package modelo;
 
 public enum EfectosCartaUno {
-	MAS_DOS,
-	MAS_CUATRO,
-	REVERSA,
-	SALTO,
-	CAMBIO_COLOR
+	MAS_DOS(20),
+	MAS_CUATRO(50),
+	REVERSA(20),
+	SALTO(20),
+	CAMBIO_COLOR(50);
+	
+	private int puntos;
+	
+	private EfectosCartaUno(int puntos) {
+		this.puntos = puntos;
+	}
+	
+	public int getPuntos() {
+		return puntos;
+	}
 }
