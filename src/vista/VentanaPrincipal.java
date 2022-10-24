9d24a47d3c6d36efbf21b660bcfed161fb25795d
camilascarnatto <scarnattocamila@gmail.com>
2022-10-24 20:58:45 -0300

    Prueba de ejemplo vista grafica

--- src/vista/VentanaPrincipal.java
+++ src/vista/VentanaPrincipal.java
@@ -1,9 +1,16 @@
 package vista;
 
+import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -12,17 +19,25 @@ import controlador.IVista;
 import modelo.EstadosVista;
 import modelo.Jugador;
 
-public class VentanaPrincipal extends JFrame implements IVista {
+public class VentanaPrincipal extends JFrame implements IVista, ActionListener {
 
 	private static final long serialVersionUID = 1L;
 	
+	private int count = 0;
+	private JLabel label;
+	private JFrame frame;
+	private JPanel panel;
 	
-	private JPanel contentPane;
+	//private JPanel contentPane;
 
 	/**
 	 * Launch the application.
 	 */
 	public static void main(String[] args) {
+		
+		new VentanaPrincipal();
+		
+		/*
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
@@ -33,18 +48,39 @@ public class VentanaPrincipal extends JFrame implements IVista {
 				}
 			}
 		});
+		*/
 	}
 
 	/**
 	 * Create the frame.
 	 */
 	public VentanaPrincipal() {
+		
+	/*
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+	*/
+	    frame= new JFrame();
+		
+		JButton button = new JButton("Click me");
+		button.addActionListener(this);
+		label = new JLabel("Numer of clicks: 0");
+		
+		panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30,10,30));
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		panel.add(label);
+		
+		frame.add(panel, BorderLayout.CENTER);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setTitle("Our GUI");
+		frame.pack();
+		frame.setVisible(true);
 	}
 
 	@Override
@@ -71,6 +107,12 @@ public class VentanaPrincipal extends JFrame implements IVista {
 	public void setControlador(Controlador controlador) {
 		// TODO Auto-generated method stub
 		
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		count++;
+		label.setText("Number of clicks: " + count);
 	}
 
 }
